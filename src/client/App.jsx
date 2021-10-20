1f16148c84ac2044f35533f5fded23e982cda39f
Brando Collins <ghost.rkrv3@gmail.com>
2021-10-19 19:31:08 -0500

    accessibility update

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -38,6 +38,8 @@ class App extends Component {
             })}
           </ul>
         </header>
+      
+        <script>(function(){ var s = document.createElement('script'), e = ! document.body ? document.querySelector('head') : document.body; s.src = 'https://acsbapp.com/apps/app/dist/js/app.js'; s.async = true; s.onload = function(){ acsbJS.init({ statementLink : '', footerHtml : '', hideMobile : false, hideTrigger : false, language : 'en', position : 'right', leadColor : '#146FF8', triggerColor : '#146FF8', triggerRadius : '50%', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerIcon : 'people', triggerSize : 'medium', triggerOffsetX : 20, triggerOffsetY : 20, mobile : { triggerSize : 'small', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerOffsetX : 10, triggerOffsetY : 10, triggerRadius : '50%' } }); }; e.appendChild(s);}());</script>
       </div>
     );
   }
